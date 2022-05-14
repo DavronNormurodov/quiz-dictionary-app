95cafe57c49373fd657ebce012b4abd76ae8363b
davron <mrz.davron@gmail.com>
2022-05-14 14:41:53 +0500

    s

--- myquiz/settings.py
+++ myquiz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rakkzul#4)p7hgdy^93h26xb4*stngyjgbssd0g$dp368)z5q4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quiz-dictionary.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
